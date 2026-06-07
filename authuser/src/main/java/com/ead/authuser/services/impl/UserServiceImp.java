75343e1a84f4314a971c8478a6cfd7f3ee225df7
Gabriel Santos <gabriel.santos.bcc@gmail.com>
2026-06-07 19:17:08 -0300

    feat: user rest

---
+++ authuser/src/main/java/com/ead/authuser/services/impl/UserServiceImp.java
@@ -0,0 +1,41 @@
+package com.ead.authuser.services.impl;
+
+import com.ead.authuser.exceptions.NotFoundException;
+import com.ead.authuser.models.UserModel;
+import com.ead.authuser.repositories.UserRepository;
+import com.ead.authuser.services.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class UserServiceImp  implements UserService {
+
+    final UserRepository userRepository;
+
+    public UserServiceImp(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<UserModel> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<UserModel> findById(UUID userId) {
+        Optional<UserModel> userModelOptional = userRepository.findById(userId);
+        if (userModelOptional.isEmpty()) {
+            throw new NotFoundException("Error: User not found.");
+        }
+        return userModelOptional;
+    }
+
+    @Override
+    public void delete(UserModel userModel) {
+        userRepository.delete(userModel);
+    }
+
+}
